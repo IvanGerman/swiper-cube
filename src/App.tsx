6f4c97f36e30e43b624bd5e3dd1cf9042310d553
IvanGerman <ivgvi@mail.ru>
2023-02-13 18:55:30 +0700

    feat: add swiper js cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import './App.css';
+
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+import { EffectCube, Pagination, Autoplay } from "swiper";
+
 
 const App: FC = () => {
   return (
-    <div className="App">
-      swiper
-    </div>
+    <>
+      <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        autoplay={ { delay: 1000 } }
+        pagination={true}
+        modules={[EffectCube, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
 
